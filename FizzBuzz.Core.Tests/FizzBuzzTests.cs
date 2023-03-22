6ffb23779d1d66567aa566cdb6302bf69d4870a3
Caleb-Geiger-Schoolsin <caleb.geiger@schoolsin.com>
2023-03-22 18:31:14 -0400

    adds fizzbuzz service and test

--- FizzBuzz.Core.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Core.Tests/FizzBuzzTests.cs
@@ -4,9 +4,39 @@ namespace FizzBuzz.Core.Tests;
 
 public class FizzBuzzTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private FizzBuzzService _fizzbuzzservice;
 
+    public FizzBuzzTests()
+    {
+        _fizzbuzzservice = new FizzBuzzService();
+    }
+
+    [Fact]
+    public void Should_Return_Number()
+    {
+        //assert
+        Assert.Equal("1", _fizzbuzzservice.Print(1));
+        Assert.Equal("2", _fizzbuzzservice.Print(2));
+    }
+
+    [Fact]
+    public void Should_Return_Fizz() 
+    {
+        Assert.Equal("Fizz", _fizzbuzzservice.Print(3));
+        Assert.Equal("Fizz", _fizzbuzzservice.Print(6));
+    }
+
+    [Fact]
+    public void Should_Return_Buzz() 
+    {
+        Assert.Equal("Buzz", _fizzbuzzservice.Print(5));
+        Assert.Equal("Buzz", _fizzbuzzservice.Print(10));
+    }
+
+    [Fact]
+    public void Should_Return_FizzBuzz() 
+    {
+        Assert.Equal("FizzBuzz", _fizzbuzzservice.Print(15));
+        Assert.Equal("FizzBuzz", _fizzbuzzservice.Print(30));
     }
 }
--- FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzz.Core/FizzBuzzService.cs
@@ -1,5 +1,29 @@
 ﻿namespace FizzBuzz.Core;
 public class FizzBuzzService
 {
+    public string Print(int number)
+    {
+        if (IsDivisibleByThree(number) && IsDivisibleByFive(number))
+        {
+            return "FizzBuzz";
+        }
+        else if (IsDivisibleByThree(number))
+        {
+            return "Fizz";
+        }
+        else if (IsDivisibleByFive(number))
+        {
+            return "Buzz";
+        }
+        return number.ToString();
+    }
 
+    private static bool IsDivisibleByFive(int number)
+    {
+        return number % 5 == 0;
+    }
+
+    private bool IsDivisibleByThree (int number) {
+        return number % 3 == 0;
+    }
 }
